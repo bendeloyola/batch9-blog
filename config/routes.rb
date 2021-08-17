79568b346d91ad25e91e7ca2087842b7a642ee83
Ben De Loyola <bendeloyola2017@gmail.com>
2021-08-17 22:06:43 +0800

    fix create button

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/articles' => 'article#index'
-  get '/articles/new' => 'article#new'
+  get '/articles/new' => 'article#new', as: 'article_new'
   post '/articles' => 'article#create', as: 'article_create'
   get '/articles/:id' => 'article#show', as: 'article_show'
   get '/articles/edit/:id' => 'article#edit', as: 'article_edit'
