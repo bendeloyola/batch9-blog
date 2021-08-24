6733a444588cf4f05c3da973aa00ba026b9add2a
Ben De Loyola <bendeloyola2017@gmail.com>
2021-08-24 17:16:32 +0800

    applying resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/articles' => 'article#index'
-  get '/articles/new' => 'article#new', as: 'article_new'
-  post '/articles' => 'article#create', as: 'article_create'
-  get '/articles/:id' => 'article#show', as: 'article_show'
-  get '/articles/edit/:id' => 'article#edit', as: 'article_edit'
-  put '/articles/:id' => 'article#update', as: 'article_update'
-  delete '/articles/:id' => 'article#delete', as: 'article_delete'
+  # get '/articles' => 'articles#index'
+  get '/articles/new' => 'articles#new', as: 'article_new'
+  post '/articles' => 'articles#create', as: 'article_create'
+  get '/articles/:id' => 'articles#show', as: 'article_show'
+  get '/articles/edit/:id' => 'articles#edit', as: 'article_edit'
+  put '/articles/:id' => 'articles#update', as: 'article_update'
+  delete '/articles/:id' => 'articles#delete', as: 'article_delete'
   get '/user' => 'user#index'
+
+  resources :articles
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_20_120618) do
+ActiveRecord::Schema.define(version: 2021_08_24_051914) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 2021_08_20_120618) do
   create_table "articles", force: :cascade do |t|
     t.string "name"
     t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "article_id"
+    t.string "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
