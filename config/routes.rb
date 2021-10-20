b4e5af55a962b2904a9dd0c6a1aa4a15a0ea2d1e
Ben De Loyola <bendeloyola2017@gmail.com>
2021-10-20 18:54:52 +0800

    update root routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # get '/articles' => 'articles#index'
+  root to: 'articles#index', as: 'articles'
   get '/articles/new' => 'articles#new', as: 'article_new'
   post '/articles' => 'articles#create', as: 'article_create'
   get '/articles/:id' => 'articles#show', as: 'article_show'
